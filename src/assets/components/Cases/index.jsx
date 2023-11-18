48462fe89aadd713d45eb320a8022823c77129dd
gabrielmulleer <emaildegabrielm@gmail.com>
2023-11-18 16:38:17 -0300

    feat: add media query for tablet in Cases component

--- src/assets/components/Cases/index.jsx
+++ src/assets/components/Cases/index.jsx
@@ -4,14 +4,16 @@ import { GoArrowRight } from 'react-icons/go';
 export default function Cases({ Company, Case, children, Img }) {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.wrapper__info}>
-        <h3 className={styles.wrapper__info_company}>{Company}</h3>
-        <h2 className={styles.wrapper__info_case}>{Case}</h2>
-        <p className={styles.wrapper__info_text}>{children}</p>
+      <div className={styles.wrapper__container}>
+        <div className={styles.wrapper__info}>
+          <h3 className={styles.wrapper__info_company}>{Company}</h3>
+          <h2 className={styles.wrapper__info_case}>{Case}</h2>
+          <p className={styles.wrapper__info_text}>{children}</p>
+        </div>
+        <a href={`/${Case.toLowerCase()}`} className={styles.wrapper__button}>
+          View <GoArrowRight size='20px' />
+        </a>
       </div>
-      <a href={`/${Case.toLowerCase()}`} className={styles.wrapper__button}>
-        View <GoArrowRight size='20px' />
-      </a>
 
       <img src={Img} alt='' className={styles.wrapper__img} />
     </div>
