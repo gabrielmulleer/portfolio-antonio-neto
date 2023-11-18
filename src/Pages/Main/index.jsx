b39509217d60e468a43573f8394e8cd17e3ea4ed
gabrielmulleer <emaildegabrielm@gmail.com>
2023-11-18 16:14:11 -0300

    feat: add Cases component style

--- src/Pages/Main/index.jsx
+++ src/Pages/Main/index.jsx
@@ -1,5 +1,6 @@
 import styles from './Main.module.scss';
 import Tag from '../../assets/components/Tag';
+import Cases from '../../assets/components/Cases';
 
 export default function Main() {
   return (
@@ -16,6 +17,22 @@ export default function Main() {
         </section>
         <section className={styles.wrapper__work}>
           <Tag>WORK</Tag>
+          <Cases
+            Company='Grupo Boticário'
+            Case='Mensageria'
+            Img='https://source.unsplash.com/random/300x300?r=2'
+          >
+            Lorem ipsum dolor sit amet consectetur. Blandit non massa
+            pellentesque velit nunc in a fringilla tortor.
+          </Cases>
+          <Cases
+            Company='Accenture'
+            Case='Consulting'
+            Img='https://source.unsplash.com/random/300x300?r=1'
+          >
+            Lorem ipsum dolor sit amet consectetur. Blandit non massa
+            pellentesque velit nunc in a fringilla tortor.
+          </Cases>
         </section>
       </main>
     </>
--- src/assets/components/Cases/index.jsx
+++ src/assets/components/Cases/index.jsx
@@ -1,15 +1,19 @@
 import styles from './Cases.module.scss';
 import { GoArrowRight } from 'react-icons/go';
 
-export default function Cases({ Company, Case, children }) {
+export default function Cases({ Company, Case, children, Img }) {
   return (
-    <div>
-      <h3>{Company}</h3>
-      <h2>{Case}</h2>
-      <p>{children}</p>
-      <a href={`/${Case.toLowerCase()}`}>
-        View <GoArrowRight />
+    <div className={styles.wrapper}>
+      <div className={styles.wrapper__info}>
+        <h3 className={styles.wrapper__info_company}>{Company}</h3>
+        <h2 className={styles.wrapper__info_case}>{Case}</h2>
+        <p className={styles.wrapper__info_text}>{children}</p>
+      </div>
+      <a href={`/${Case.toLowerCase()}`} className={styles.wrapper__button}>
+        View <GoArrowRight size='20px' />
       </a>
+
+      <img src={Img} alt='' className={styles.wrapper__img} />
     </div>
   );
 }
