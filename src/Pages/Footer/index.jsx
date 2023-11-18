e80bfc0e8b41e71090bb4ab75a64823391c401ab
gabrielmulleer <emaildegabrielm@gmail.com>
2023-11-18 13:11:54 -0300

    feat-add-style

--- src/Pages/Footer/index.jsx
+++ src/Pages/Footer/index.jsx
@@ -1,11 +1,12 @@
 import { MdOutlineMailOutline } from 'react-icons/md';
 import { AiOutlineLinkedin } from 'react-icons/ai';
+import styles from './Footer.module.scss';
 
 export default function Footer() {
   return (
-    <footer>
-      <h3>© 2023 Antonio Neto</h3>
-      <div>
+    <footer className={styles.wrapper}>
+      <h3 className={styles.wrapper__copyright}>© 2023 Antonio Neto</h3>
+      <div className={styles.wrapper__icons}>
         <AiOutlineLinkedin size='40px' color='#4F4F4F' />
         <MdOutlineMailOutline size='40px' color='#4F4F4F' />
       </div>
