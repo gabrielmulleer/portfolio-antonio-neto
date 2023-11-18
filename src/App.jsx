4be7ed2ed7802c0c8d572f0d52873619280f93e3
gabrielmulleer <emaildegabrielm@gmail.com>
2023-11-18 10:22:06 -0300

    feat-add-header-main-footer-folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Header from './Pages/Header';
+import Main from './Pages/Main';
+import Footer from './Pages/Footer';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
