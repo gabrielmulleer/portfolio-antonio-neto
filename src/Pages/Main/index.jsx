f94fa2478b947ebefdab335c335edec62b88a47e
gabrielmulleer <emaildegabrielm@gmail.com>
2023-11-18 14:28:52 -0300

    feat-add-who-section

--- src/Pages/Main/index.jsx
+++ src/Pages/Main/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Tag from '../../assets/components/Tag';
 
 export default function Main() {
   return (
-    <div>
-      <h1>AntonioNeto</h1>
-      <p> é txonga </p>
-    </div>
+    <>
+      <section>
+        <Tag>WHO</Tag>
+        <p>I’m a Product designer. </p>
+        <p>Based in Brazil, currently designing at Grupo Boticário.</p>
+      </section>
+      <section>
+        <Tag>WORK</Tag>
+      </section>
+    </>
   );
 }
