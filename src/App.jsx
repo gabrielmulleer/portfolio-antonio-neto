30e9203145c0b80d6c67249efa9a862ff22cf47c
gabrielmulleer <emaildegabrielm@gmail.com>
2023-11-18 12:55:12 -0300

    feat-add-responsive-menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
+import './App.css';
 import Header from './Pages/Header';
 import Main from './Pages/Main';
 import Footer from './Pages/Footer';
 
 function App() {
   return (
-    <>
+    <div className='app'>
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   );
 }
 
--- src/Pages/Header/index.jsx
+++ src/Pages/Header/index.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import styles from './Header.module.scss';
 import { IoIosMenu } from 'react-icons/io';
 
 export default function Header() {
   return (
-    <div className={styles.wrapper}>
+    <header className={styles.wrapper}>
       <div className={styles.wrapper__logo}>
         <img
           className={styles.wrapper__logo_svg}
@@ -13,7 +12,21 @@ export default function Header() {
         />
         <h3 className={styles.wrapper__logo_title}>Antônio Neto</h3>
       </div>
-      <IoIosMenu size='30px' color='#333' />
-    </div>
+      <IoIosMenu size='30px' color='#333' className={styles.wrapper__icon} />
+
+      <nav className={styles.menu}>
+        <ul className={styles.menu__dropdown}>
+          <li>
+            <a href='#'>Home</a>
+          </li>
+          <li>
+            <a href='#'>About</a>
+          </li>
+          <li>
+            <a href='#'>Work</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
