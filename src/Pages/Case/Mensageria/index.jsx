db2ae818be2430867c1eb151b2deb947e27b8554
gabrielmulleer <emaildegabrielm@gmail.com>
2023-11-23 22:35:43 -0300

    feat: initialyze mensageria case

---
+++ src/Pages/Case/Mensageria/index.jsx
@@ -0,0 +1,10 @@
+import styles from './Mensageria.module.scss';
+export default function Mensageria() {
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <img src='#' alt='Mensageria Banner' />
+      </div>
+    </>
+  );
+}
